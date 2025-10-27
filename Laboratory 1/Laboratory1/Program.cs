2d6d985877952899b64627d8d1bdffd0dc0c9079
Ardelean Luca Teodor <ardeleanluca@Ardeleans-MacBook-Pro.local>
2025-10-27 09:34:40 +0200

    Add Laboratory 1 project and .gitignore

---
+++ Laboratory 1/Laboratory1/Program.cs
@@ -0,0 +1,20 @@
+﻿        // Citim numarul de la tastatura
+        Console.Write("Introduceti numarul: ");
+        string numar = Console.ReadLine();
+
+        // Citim baza initiala
+        Console.Write("Introduceti baza initiala (2-16): ");
+        int bazaInitiala = int.Parse(Console.ReadLine());
+
+        // Citim baza finala
+        Console.Write("Introduceti baza finala (2-16): ");
+        int bazaFinala = int.Parse(Console.ReadLine());
+
+        // Convertim numarul in baza 10 (intermediar)
+        int valoareInBaza10 = Convert.ToInt32(numar, bazaInitiala);
+
+        // Convertim baza 10 in baza finala
+        string rezultat = Convert.ToString(valoareInBaza10, bazaFinala).ToUpper();
+
+        // Afisam rezultatul
+        Console.WriteLine($"Numarul {numar} din baza {bazaInitiala} este {rezultat} in baza {bazaFinala}");
